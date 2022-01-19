dfa7564b6bb4b94996c6a98090c2ce7595cba4ce
Pedro Narciso García Revington <p.revington@gmail.com>
2022-01-19 19:00:45 +0100

    test expires + revamp to use modern javascript

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
+# zd-capability-param
+
+Library to create, validate and decode [capability url](https://www.w3.org/TR/capability-urls/) tokens.  
 
 ![Build Status](https://github.com/revington/zd-capability-param/actions/workflows/node.js.yml/badge.svg)
 
 [![Known Vulnerabilities](https://snyk.io/test/github/revington/zd-capability-param/badge.svg?targetFile=package.json)](https://snyk.io/test/github/revington/zd-capability-param?targetFile=package.json)
 [![Coverage Status](https://coveralls.io/repos/github/revington/zd-capability-param/badge.svg?branch=master)](https://coveralls.io/github/revington/zd-capability-param?branch=master)
-# zd-capability-param
 
-Library to create, validate and decode [capability url](https://www.w3.org/TR/capability-urls/) tokens.  
 You know, this "click on this link to validate your email account" or "click to recover your account".  
 Generated tokens are:
 * Difficult to guess/tamper
 * Limited lifetime
-* URL not-unfriendly 
+* Valid URL
 
 ### Difficult to guess/tamper
 
@@ -25,8 +26,8 @@ The link includes an expiration date. This expiration date is included in the HM
 The HMAC secret could be easily rotated by just "pushing" more secrets. Each secret will be tested if the TTL looks good. For this reason it is important to set a low `maxSecrets` number (i.e 5).  
 Older secrets will get discarded.
 
-### URL not-unfriendly
-Token is kept short enough by encoding it as Base64. This Base64 string is also URL-encoded to prevent unfriendly url characters like %, /, ? or &
+### Valid URL
+Token is kept short enough by encoding it as Base64. This Base64 string is also URL-encoded to prevent breaking urls i.e by adding `&`.
 .
 ## Install
 ```
@@ -39,13 +40,33 @@ $ npm install zd-capability-param
 
 ```javascript
 // create a token valid for 48 hours
-const lifetime = 2 * 24 * 60 * 60 * 1000; // 48 HOURS
+const lifetime = 2 * 24 * 60; // 48 HOURS
 const param = capabilityParam.create('secret', {lifetime});
+const toEncode = {user: 'alice@localhost'};
 // encode
-const token = param.encode({ validateEmail: user.email });
+const token = param.encode(toEncode);
+console.log('token', token);
+// prints
+// token 27376691.eyJ1c2VyIjoiYWxpY2VAbG9jYWxob3N0In0%3D.7cf77z2h%2Bz0cR%2Brc2hVO5L8cV7Q05pfanwMxqHTM9Qc%3D
 
 // decode
-const payload = param.decode(token);
+let result;
+try{
+	result = param.decode(token);
+}catch(e){
+	if(e instanceof ExpiredCapabilityError){
+	// capability param has expired i.e link is too old
+		console.log(`token expired at ${e.expired}`);
+		return;
+	}
+	if(e instanceof InvalidCapabilityError){
+		// capability param has expired i.e link is too old
+		console.log(`capability is not valid: "${e.capabilityParam}"`);
+		return;
+	}
+}
+console.log('user', result.user);
+// prints user alice@localhost
 ```
 Check [express example](examples/express.js)
 
@@ -55,7 +76,7 @@ Check [express example](examples/express.js)
 
 * `#secret` Could be an string or an array of strings
 * `#options` Optional
-	* `lifetime` Token lifetime in miliseconds. Default is 48 hours.
+	* `lifetime` Token lifetime in minutes. Default is 48 hours.
 	* `algorithm` A hash algoritm. Default `sha256`.
 	* `maxSecrets` Max number of secrets to keep. See [secret rotation](#secret-rotation). Default is `5`.
 * Returns `{encode(payload), decode(token), pushSecret(secret)}`
@@ -64,7 +85,7 @@ The algorithm is dependent on the available algorithms supported by the version 
 
 
 ### Secret rotation
-```
+```javascript
 // Provide a secret set on creation
 const secrets = ['currentSecret','oldSecret'];
 const param = capabilityParam.create(secrets, {maxSecrets:2});
@@ -74,6 +95,6 @@ param.pushSecret('new current secret');
 // Valid secrets are 'new current secret'  and 'currentSecret'
 // New tokens are signed with 'new current secret'
 ```
+## Security warning
 
-
-
+The `payload` is signed with HMAC and encoded to Base64 but IT IS NOT ENCRYPTED. Anyone can "see" the contents of the payload by decoding the string.
--- index.js
+++ index.js
@@ -1,72 +1,141 @@
 'use strict';
-const {
-    URL
-} = require('url');
-const HOUR = 60 * 60 * 1000;
+/**
+ * zd-capability-param
+ * @module zd-capability-param
+ * @see module:zd-capability-param
+ */
+
 const message = require('./lib/message');
 const crypto = require('crypto');
 const secretStore = require('./lib/secret-store');
 const assert = require('assert');
 const now = require('./lib/now');
 
-function sign(secret, message, algorithm) {
-    const hash = crypto.createHmac(algorithm, secret);
-    hash.update(message);
-    return hash.digest('base64');
+/**
+ * ExpiredCapabilityError.
+ *
+ * @extends {Error}
+ * @property {string} capabilityParam - The expired capability param
+ * @property {Date} expired - Date this capability param expired
+ */
+class ExpiredCapabilityError extends Error {
+	constructor(capabilityParam, expired) {
+		super('expired');
+		this.expired = expired;
+		this.capabilityParam = capabilityParam;
+		this.name = this.constructor.name;
+	}
 }
-const defaults = {
-    lifetime: HOUR * 48,
-    algorithm: 'sha256',
-    maxSecrets: 5
-};
+/**
+ * InvalidCapabilityError.
+ *
+ * @extends {Error}
+ * @property {string} capabilityParam - The invalid capability param
+ */
+class InvalidCapabilityError extends Error {
+	constructor(capabilityParam) {
+		super('invalid signature');
+		this.capabilityParam = capabilityParam;
+		this.name = this.constructor.name;
+	}
+}
+/**
+ * Sign a string with HMAC
+ *
+ * @param {string} secret - Secret to sign the message
+ * @param {string} message - String to be signed
+ * @param {string} [algorithm=sha256] - Algorithm
+ * @returns {string} Base64 HMAC signature
+ */
+function sign(secret, message, algorithm) {
+	const hash = crypto.createHmac(algorithm, secret);
+	hash.update(message);
+	return hash.digest('base64');
+}
 
 function createSecrets(maxSecrets, ...secrets) {
-    const ret = secretStore(maxSecrets);
-    secrets.forEach(x => ret.push(x));
-    return ret;
+	const ret = secretStore(maxSecrets);
+	secrets.forEach(x => ret.push(x));
+	return ret;
 }
 
+class Capability {
+	#algorithm
+	#lifetime
+	#secrets
+	constructor(secret, options) {
+		this.#lifetime = options.lifetime;
+		this.#algorithm = options.algorithm;
+		this.#secrets = createSecrets(options.maxSecrets, secret);
+	}
+	/**
+	 * Signs the payload with HMAC
+	 *
+	 * @param {string} input - String to sign
+	 * @returns {string} Signature 
+	 */
+	encode(payload) {
+		const validUntil = now() + this.#lifetime;
+		const myMessage = message.stringify(validUntil, payload);
+		const signature = sign(this.#secrets.getCurrentSecret(), myMessage, this.#algorithm);
+		return encodeURIComponent(message.format(myMessage, signature));
+	}
+	decode(input) {
+		input = decodeURIComponent(input);
+
+		const {
+			validUntil,
+			signedMessage,
+			payload,
+			signature
+		} = message.parse(input);
+		if (validUntil < now()) {
+			throw new ExpiredCapabilityError(input, new Date(validUntil * 60 * 1000));
+		}
+		// Try all valid secrets
+		for (let s of this.#secrets.validSecrets) {
+			if (sign(s, signedMessage, this.#algorithm) === signature) {
+				return message.fromString(payload);
+			}
+		}
+		throw new InvalidCapabilityError(input);
+	}
+	pushSecret(secret) {
+		this.#secrets.push(secret);
+	}
+}
+
+/**
+ * create.
+ *
+ * @param {string} secret - Secret to sign
+ * @param {Object} options
+ * @param {string} [options.lifetime=2880] - Lifetime expresed in minutes. Default is 48 hours
+ * @param {string} [options.algorithm=sha256] - Algorithm used for the signature
+ * @param {number} [options.maxSecrets=5] - Max valid secrets to keep
+ * @returns {Capability}
+ */
 function create(secret, options) {
-    var {
-        lifetime,
-        algorithm,
-        maxSecrets
-    } = Object.assign({}, options || {}, defaults);
-    lifetime = Number(lifetime);
-    const secrets = createSecrets(maxSecrets, secret);
+	assert(secret, 'secret is required');
 
-    function encode(payload) {
-        const validUntil = now() + lifetime;
-        const myMessage = message.stringify(validUntil, payload);
-        const signature = sign(secrets.getCurrentSecret(), myMessage, algorithm);
-        return encodeURIComponent(message.format(myMessage, sign(secrets.getCurrentSecret(), myMessage, algorithm)));
-    }
+	const config = {
+		lifetime: 48 * 60,
+		algorithm: 'sha256',
+		maxSecrets: 5,
+		...options
+	};
 
-    function decode(input) {
-        input = decodeURIComponent(input);
-        const {
-            validUntil,
-            signedMessage,
-            payload,
-            signature
-        } = message.parse(input);
-        if (validUntil < now()) {
-            throw new Error('timeout');
-        }
-        // Try all valid secrets
-        for (let s of secrets.validSecrets) {
-            if (sign(s, signedMessage, algorithm) === signature) {
-                return message.fromString(payload);
-            }
-        }
-        throw new Error('invalid signature');
-    }
-    return Object.freeze({
-        encode,
-        decode,
-        pushSecret: secrets.push
-    });
+	config.lifetime = Number(config.lifetime);
+	config.maxSecrets = Number(config.maxSecrets);
+
+	assert(!Number.isNaN(config.lifetime), 'lifetime must be a number');
+	assert(config.algorithm, 'algorithm must be a valid algorithm');
+	assert(!Number.isNaN(config.maxSecrets), 'maxSecrets must be a number');
+
+	return new Capability(secret, config);
 }
-exports = module.exports = {
-    create
+module.exports = {
+	create,
+	ExpiredCapabilityError,
+	InvalidCapabilityError
 };
